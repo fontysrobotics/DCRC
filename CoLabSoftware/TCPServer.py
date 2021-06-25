414307ec87f28f22a456a6e8f1c10e1c277f7762
BozhidarBonev <bozhidarbonev@gmail.com>
2021-06-25 11:17:01 +0200

    get preprocessed data via tcp server

--- CoLabSoftware/TCPServer.py
+++ CoLabSoftware/TCPServer.py
@@ -1,10 +1,15 @@
 import socketserver, pickle
 from CoLAB_main import AGVMap
+from CoLAB_sendLocation import preProcessedLocation,preProcessedOrientation
+
 class TCPServer(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(1024).strip()
         if data == b'List':
             self.request.sendall(pickle.dumps(AGVMap))
+        elif data == b'Preprocessed':
+            toSend = [preProcessedLocation,preProcessedOrientation]
+            self.request.sendall(pickle.dumps(toSend))
         else:
             self.request.sendall(b'Default')
         
